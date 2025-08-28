d4535bb752829db9222172b7008e403b02f2f6d1
PRAVIN M <137031106+pravin2303@users.noreply.github.com>
2025-08-28 22:28:33 +0530

    Create longestCommonPrefix.py

---
+++ longestCommonPrefix.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:  # if list is empty
+            return ""
+
+        # Take the first string as starting prefix
+        prefix = strs[0]
+
+        # Compare with each other string
+        for s in strs[:]:
+            # While current string does not start with prefix
+            while not s.startswith(prefix):
+                # Shorten prefix by removing last char
+                prefix = prefix[:-1]
+                if prefix == "":
+                    return ""   # no common prefix at all
+        
+        return prefix
