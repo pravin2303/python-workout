67e4affcf1af2bfa879cdcb0ffdbcec6ef17e086
PRAVIN M <137031106+pravin2303@users.noreply.github.com>
2025-09-01 22:41:15 +0530

    Create 268. Missing Number.py

---
+++ EASY/268. Missing Number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n =len(nums)
+        exp_sum = n*(n+1)//2
+        actual_sum =sum(nums)
+        return exp_sum - actual_sum
+
+        
