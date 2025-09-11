729602b3492ba61018371821a06267babedbc78d
PRAVIN M <137031106+pravin2303@users.noreply.github.com>
2025-09-11 20:09:23 +0530

    Create 125. Valid Palindrome.py

---
+++ EASY/125. Valid Palindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        filter=''.join(ch.upper()for ch in s if ch.isalnum())
+        left,right=0,len(filter)-1
+        while left<right:
+            if filter[left]!=filter[right]:
+                return False
+            left +=1
+            right -=1
+        return True
