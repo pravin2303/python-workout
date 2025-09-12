29ca3252f572d2e1fdf0f9c468a2a2db68ea4cc6
PRAVIN M <137031106+pravin2303@users.noreply.github.com>
2025-09-12 19:35:51 +0530

    Create 459. Repeated Substring Pattern.py

---
+++ EASY/459. Repeated Substring Pattern.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        #return s in (s+s)[1:-1]
+        n = len(s)
+        for i in range(1,n//2+1):
+            if n%i ==0:
+                sub=s[:i]
+                if sub *(n//i)==s:
+                    return True
+        return False
